cdf14cbd3044da1bc9a0c42f174a764d61428061
Philipp Schlatter <pschlatt@mech.kth.se>
2025-01-14 18:05:41 +0100

    added mpi tests

---
+++ mpi/hello.py
@@ -0,0 +1,14 @@
+# run this via: mpirun -np 4 python hello.py
+
+
+
+from mpi4py import MPI
+import sys
+
+size = MPI.COMM_WORLD.Get_size()
+rank = MPI.COMM_WORLD.Get_rank()
+name = MPI.Get_processor_name()
+
+sys.stdout.write(
+    "Hello, World! I am process %d of %d on %s.\n"
+    % (rank, size, name))
